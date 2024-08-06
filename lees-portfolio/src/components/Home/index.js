a5216a04f372c092a351da148287385efb31a86e
Lee_D <leegreat21@gmail.com>
2024-08-06 17:37:30 +0300

    homePage

---
+++ lees-portfolio/src/components/Home/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import leeLogo from '../../assets/images/Lee_logo.png'
+import { Link } from 'react-router-dom'
+import './index.scss'
+
+const Home = () => {
+  return (
+    <div className="container homePage">
+      <div className="textZone">
+        <h1>
+          Hi,
+          <br />
+          I'm
+          <img src={leeLogo} alt="developer" />
+          web developer
+        </h1>
+        <h2>Full-Stack Developer</h2>
+        <Link to="contact" className="flat-button">
+          Contact Me
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
