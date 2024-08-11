5a9959b011d4cbd95542564e08379e1cb1f4cd3c
Lee_D <leegreat21@gmail.com>
2024-08-11 10:31:24 +0300

    fixing-animation

--- lees-portfolio/src/components/Home/index.js
+++ lees-portfolio/src/components/Home/index.js
@@ -1,20 +1,53 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import leeLogo from '../../assets/images/Lee_logo.png'
 import { Link } from 'react-router-dom'
 import './index.scss'
+import '../../App.scss'
 import '../Layout/index.scss'
+import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animation')
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+  ]
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <div className="container homePage">
       <div className="textZone">
         <h1>
-          Hi,
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
           <br />
-          I'm
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+
           <img src={leeLogo} alt="developer" />
           <br />
-          web developer
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={0}
+          />
         </h1>
         <h2>Full-Stack Developer</h2>
         <Link to="contact" className="flat-button">
