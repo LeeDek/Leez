182d117ba0a069c8f051409dd174a4bdf3e46b71
Lee_D <leegreat21@gmail.com>
2024-11-01 12:20:24 +0200

    form correction

--- lees-portfolio/src/components/Contact/index.js
+++ lees-portfolio/src/components/Contact/index.js
@@ -1,7 +1,78 @@
-import React from 'react'
+import Loader from 'react-loaders'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useState, useEffect } from 'react'
 
-const index = () => {
-  return <div>index</div>
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+      console.log('letterClass updated to text-animate-hover')
+    }, 4000)
+
+    return () => clearTimeout(timeoutId)
+  }, [])
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', '', 'M', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm excited to connect and collaborate with individuals or teams who
+            share a passion for building impactful digital solutions. With a
+            strong foundation in web and mobile development, I approach each
+            project with a creative mindset and a focus on quality. For project
+            inquiries, questions, or just to connect, please feel free to reach
+            out using the form below.
+          </p>
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="Send" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+      <Loader type="square-spin" />
+    </>
+  )
 }
 
-export default index
+export default Contact
